beebb5ce8d539a84015c9e7f33c1618941ef5dde
Harshit Raghuvanshi <rharshit98@gmail.com>
2025-01-18 15:29:47 +0530

    CarSync: Initialize

---
+++ carsync/src/main/java/com/rharshit/carsync/CarSyncApplication.java
@@ -0,0 +1,13 @@
+package com.rharshit.carsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSyncApplication.class, args);
+    }
+
+}
---
+++ carsync/src/test/java/com/rharshit/carsync/CarSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rharshit.carsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
