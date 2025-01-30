3c036f71823da914e1d962abf55104b5f032b35c
Harshit Raghuvanshi <rharshit98@gmail.com>
2025-01-30 20:09:16 +0530

    CarSync: Handle error while fetching car list

--- carsync/src/main/java/com/rharshit/carsync/service/CarService.java
+++ carsync/src/main/java/com/rharshit/carsync/service/CarService.java
@@ -2,11 +2,14 @@ package com.rharshit.carsync.service;
 
 import com.rharshit.carsync.repository.CarModelRepository;
 import com.rharshit.carsync.repository.model.CarModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class CarService {
 
@@ -14,6 +17,11 @@ public class CarService {
     private CarModelRepository carModelRepository;
 
     public List<CarModel> getCars() {
-        return carModelRepository.findAll();
+        try {
+            return carModelRepository.findAll();
+        } catch (Exception e) {
+            log.error("Error fetching list of cars", e);
+            return Collections.emptyList();
+        }
     }
 }
