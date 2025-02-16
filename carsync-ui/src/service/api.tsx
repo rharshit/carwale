0d417716b6184801af1162ed431fa436730f2ca0
Harshit Raghuvanshi <rharshit98@gmail.com>
2025-02-16 18:01:39 +0530

    UI: Add API service module for handling HTTP requests with CORS support

---
+++ carsync-ui/src/service/api.tsx
@@ -0,0 +1,32 @@
+const baseUrl = 'http://localhost:8080/api';
+
+const call = (url: string, method: string, data: any) => {
+    const options: RequestInit = {
+        method: method,
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        }),
+    };
+    if (method != 'GET') {
+        options.body = JSON.stringify(data);
+    }
+    return fetch(baseUrl + url, options);
+}
+
+export const get = (url: string) => {
+    return call(url, 'GET', {});
+}
+
+export const post = (url: string, data: any) => {
+    return call(url, 'POST', data);
+}
+
+export const put = (url: string, data: any) => {
+    return call(url, 'PUT', data);
+}
+
+export const del = (url: string, data: any) => {
+    return call(url, 'DELETE', data);
+}
