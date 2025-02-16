6aaacf59cb619455d9cc63bcbf547a020b865b1a
Harshit Raghuvanshi <rharshit98@gmail.com>
2025-02-16 17:55:08 +0530

    CarSync: API - Enable CORS

---
+++ carsync/src/main/java/com/rharshit/carsync/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.rharshit.carsync.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        // TODO: Allow only trusted origins
+//                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- carsync/src/main/java/com/rharshit/carsync/config/SpringConfig.java
+++ carsync/src/main/java/com/rharshit/carsync/config/SpringConfig.java
@@ -1,4 +1,4 @@
-package com.rharshit.carsync.common;
+package com.rharshit.carsync.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
