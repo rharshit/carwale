533ce385eb80812fd763424248853fbc8d7ff67c
Harshit Raghuvanshi <rharshit98@gmail.com>
2025-02-16 22:33:54 +0530

    CarSync: UI - Wrap MainLayout in ConfigProvider with custom theme settings

--- carsync-ui/src/App.tsx
+++ carsync-ui/src/App.tsx
@@ -1,11 +1,20 @@
+import { ConfigProvider } from "antd";
 import MainLayout from "./pages/MainLayout";
 
 function App() {
 
   return (
-    <>
+    <ConfigProvider theme={{
+      token: {
+        borderRadius: 16,
+        borderRadiusXS: 8,
+        borderRadiusSM: 16,
+        borderRadiusLG: 32,
+        borderRadiusOuter: 16
+      }
+    }}>
       <MainLayout />
-    </>
+    </ConfigProvider>
   )
 }
 
